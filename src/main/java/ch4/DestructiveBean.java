697a5c566a2447d8e683ffaf6e0b6a09a956dcff
avhimkov <avhimkov@mail.ru>
2017-02-28 01:32:35 +0500

    add DestructiveBean class and refactoring xml

---
+++ src/main/java/ch4/DestructiveBean.java
@@ -0,0 +1,48 @@
+package ch4;
+
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+import java.io.File;
+
+public class DestructiveBean implements InitializingBean {
+
+    private File file;
+    private String filePath;
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        System.out.println("Initializing Bean");
+        if (filePath == null){
+            throw new IllegalArgumentException("You must specify the filePath property of " + DestructiveBean.class);
+        }
+        this.file = new File(filePath);
+        this.file.createNewFile();
+
+        System.out.println("File exist"+file.exists());
+    }
+
+    public void destroy(){
+        System.out.println("Destroy Bean");
+
+        if (!file.delete()){
+            System.err.println("ERROR: failed to delete file.");
+        }
+        System.out.println("File exist"+file.exists());
+    }
+
+    public void setFilePath(String filePath){
+        this.filePath = filePath;
+    }
+
+    public static void main(String[] args) {
+        GenericXmlApplicationContext ctx = new GenericXmlApplicationContext();
+        ctx.load("classpath:META-INF/app-context-xml.xml");
+        ctx.refresh();
+
+        DestructiveBean bean = (DestructiveBean) ctx.getBean("destructiveBean");
+        System.out.println("Calling destroy ()");
+        ctx.destroy();
+        System.out.println("Called destroy ()");
+    }
+}
