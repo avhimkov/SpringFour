146ef8959f53303a0c7271f95083b7f0641bbe07
avhimkov <avhimkov@mail.ru>
2017-03-05 00:47:03 +0500

    refactoring xml

--- src/main/java/ch4/NamePropertyEditor.java
+++ src/main/java/ch4/NamePropertyEditor.java
@@ -4,9 +4,8 @@ import java.beans.PropertyEditorSupport;
 
 public class NamePropertyEditor extends PropertyEditorSupport {
     @Override
-    public void setAsText(String text) throws IllegalArgumentException{
+    public void setAsText(String text) throws IllegalArgumentException {
         String[] name = text.split("\\s");
         setValue(new Name(name[0], name[1]));
     }
-
 }
