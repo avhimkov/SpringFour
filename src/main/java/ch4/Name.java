941839846ecf9e68937951e174ae90305b6ab8d7
avhimkov <avhimkov@mail.ru>
2017-03-05 00:31:04 +0500

    add class Name

---
+++ src/main/java/ch4/Name.java
@@ -0,0 +1,26 @@
+package ch4;
+
+public class Name {
+    private String firstName;
+    private String lastName;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String toString() {
+        return "First name: " + firstName + "- Last name " + lastName;
+    }
+}
