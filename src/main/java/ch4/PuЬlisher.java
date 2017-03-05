df6ec9746c9b5188a90fdbe7c652ef514b124e6d
avhimkov <avhimkov@mail.ru>
2017-03-06 00:27:55 +0500

    add class Publisher

---
+++ src/main/java/ch4/PuЬlisher.java
@@ -0,0 +1,27 @@
+package ch4;
+
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Publisher implements ApplicationContextAware {
+
+    private ApplicationContext ctx;
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        this.ctx = applicationContext;
+    }
+
+    public void publish(String message){
+        ctx.publishEvent(new MessageEvent(this, message));
+    }
+
+    public static void main(String[] args) {
+        ApplicationContext ctx = new ClassPathXmlApplicationContext("classpath:META-INF/app-context=xml.xml");
+        Publisher pub = (Publisher) ctx.getBean("publisher");
+        pub.publish("Hello World");
+        pub.publish("The quick brown fox j umped over the lazy dog");
+    }
+}
