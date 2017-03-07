ab10992c24c6d03fc8dad2b66c8744fc756f7967
avhimkov <avhimkov@mail.ru>
2017-03-08 00:55:10 +0500

    add class ProfileXmlConfigExample

---
+++ src/main/java/ch4/ProfileXmlConfigExample.java
@@ -0,0 +1,20 @@
+package ch4;
+
+import org.springframework.context.support.GenericXmlApplicationContext;
+import java.util.List;
+
+public class ProfileXmlConfigExample {
+    public static void main(String[] args) {
+        GenericXmlApplicationContext ctx = new GenericXmlApplicationContext();
+        ctx.load("classpath:META-INF/*-config.xml");
+        ctx.refresh();
+
+        FoodProviderService foodProviderService = ctx.getBean("foodProviderService", FoodProviderService.class);
+
+        List<Food> lunchSet = foodProviderService.provideLunchSet();
+
+        for (Food food : lunchSet){
+            System.out.println("Food: " + food.getName());
+        }
+    }
+}
