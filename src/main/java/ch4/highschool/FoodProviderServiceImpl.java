d9f85872f9b978dad5f2c39da7c0da2843966847
avhimkov <avhimkov@mail.ru>
2017-03-08 00:33:31 +0500

    add class FoodProviderService and xml for highschool

---
+++ src/main/java/ch4/highschool/FoodProviderServiceImpl.java
@@ -0,0 +1,18 @@
+package ch4.highschool;
+
+import ch4.Food;
+import ch4.FoodProviderService;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FoodProviderServiceImpl implements FoodProviderService {
+    @Override
+    public List<Food> provideLunchSet() {
+        List<Food> lunchSet = new ArrayList<Food>();
+        lunchSet.add(new Food("Coke"));
+        lunchSet.add(new Food("Hamburger"));
+        lunchSet.add(new Food("French Fries"));
+        return lunchSet;
+    }
+}
