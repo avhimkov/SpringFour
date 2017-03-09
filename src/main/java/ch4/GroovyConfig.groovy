dc0e31b5f241d45fce01b3493332d11736fd3a93
avhimkov <avhimkov@mail.ru>
2017-03-10 00:09:32 +0500

    add GroovyConfig.groovy

---
+++ src/main/java/ch4/GroovyConfig.groovy
@@ -0,0 +1,14 @@
+package ch4
+
+import org.springframework.beans.factory.groovy.GroovyBeanDefinitionReader
+import org.springframework.context.support.GenericApplicationContext
+
+def ctx = new GenericApplicationContext()
+def reader = new GroovyBeanDefinitionReader(ctx)
+
+reader.beans{
+    contact(Contact, firstName: 'Chris', lastName: 'Schaefer', age: 32)
+}
+ctx.refresh()
+
+println ctx.getBean("contact")
