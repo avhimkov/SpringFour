eb400d4859010a06516245f075169e5fcd96ad04
avhimkov <avhimkov@mail.ru>
2017-03-11 21:26:49 +0500

    add SecureBean class

---
+++ src/main/java/ch5/SecureBean.java
@@ -0,0 +1,8 @@
+package ch5;
+
+public class SecureBean {
+    public void writeSecurutyMessage(){
+        System.out.println("println(\"Every time I learn something new, " +
+                "it pushes some old stuff out of my brain");
+    }
+}
