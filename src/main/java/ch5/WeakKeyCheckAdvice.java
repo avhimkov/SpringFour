461f988e0e16cb487ff49560c35751f653071e64
avhimkov <avhimkov@mail.ru>
2017-03-13 00:42:21 +0500

    add AfterAdviceExample class

--- src/main/java/ch5/WeakKeyCheckAdvice.java
+++ src/main/java/ch5/WeakKeyCheckAdvice.java
@@ -8,7 +8,7 @@ public class WeakKeyCheckAdvice implements AfterReturningAdvice {
     public void afterReturning(Object returnValue, Method method, Object[] args, Object target) throws Throwable {
         if ((target instanceof KeyGenerator) && ("getKey".equals(method.getName()))){
             long key = ((Long)returnValue).longValue();
-            if (key == KeyGenerator.WEAK_KEY){
+            if (key == KeyGenerator.WEAK_KE){
                 throw new SecurityException("Кеу Generator generated а weak key. Try again");
             }
         }
