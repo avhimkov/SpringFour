7d5ffd40e0616dfa274fe692e8da2dd29dfc266e
avhimkov <avhimkov@mail.ru>
2017-03-13 00:36:40 +0500

    add WeakKeyCheckAdvice class

---
+++ src/main/java/ch5/WeakKeyCheckAdvice.java
@@ -0,0 +1,17 @@
+package ch5;
+
+import org.springframework.aop.AfterReturningAdvice;
+import java.lang.reflect.Method;
+
+public class WeakKeyCheckAdvice implements AfterReturningAdvice {
+    @Override
+    public void afterReturning(Object returnValue, Method method, Object[] args, Object target) throws Throwable {
+        if ((target instanceof KeyGenerator) && ("getKey".equals(method.getName()))){
+            long key = ((Long)returnValue).longValue();
+            if (key == KeyGenerator.WEAK_KEY){
+                throw new SecurityException("Кеу Generator generated а weak key. Try again");
+            }
+        }
+
+    }
+}
