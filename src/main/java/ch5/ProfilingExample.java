6a5e3a93b3e0b9c7aa101d45d270044ffb822d0d
avhimkov <avhimkov@mail.ru>
2017-03-14 00:25:15 +0500

    add ProfilingExample class

---
+++ src/main/java/ch5/ProfilingExample.java
@@ -0,0 +1,18 @@
+package ch5;
+
+import org.springframework.aop.framework.ProxyFactory;
+
+public class ProfilingExample {
+    public static void main(String[] args) {
+        WorkerBean bean = getWorkerBean();
+        bean.doSomeWork(1000000000);
+    }
+
+    private static WorkerBean getWorkerBean(){
+        WorkerBean target =new WorkerBean();
+        ProxyFactory factory = new ProxyFactory();
+        factory.setTarget(target);
+        factory.addAdvice(new ProfilingInterceptor());
+        return (WorkerBean) factory.getProxy();
+    }
+}
