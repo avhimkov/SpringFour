4962e4e40d0e94b5f721e3cf85e33ee38f12155d
avhimkov <avhimkov@mail.ru>
2017-03-19 01:58:12 +0500

    add TestPointcut class

---
+++ src/main/java/ch5/TestPointcut.java
@@ -0,0 +1,11 @@
+package ch5;
+
+import org.springframework.aop.support.StaticMethodMatcherPointcut;
+import java.lang.reflect.Method;
+
+public class TestPointcut extends StaticMethodMatcherPointcut {
+    @Override
+    public boolean matches(Method method, Class<?> aClass) {
+        return ("advised".equals(method.getName()));
+    }
+}
