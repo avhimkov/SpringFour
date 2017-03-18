3341373d6f3e71526243d039706f39b5311f56ad
avhimkov <avhimkov@mail.ru>
2017-03-18 23:24:04 +0500

    add DefaultSimpleBean class

---
+++ src/main/java/ch5/DefaultSimpleBean.java
@@ -0,0 +1,15 @@
+package ch5;
+
+public class DefaultSimpleBean implements SimpleBean {
+    private long dummy;
+
+    @Override
+    public void advised() {
+        dummy = System.currentTimeMillis();
+    }
+
+    @Override
+    public void unadvised() {
+        dummy = System.currentTimeMillis();
+    }
+}
