d7c8db14084ad1dd0380ae9dd40af96b571b832a
avhimkov <avhimkov@mail.ru>
2017-03-20 15:53:50 +0500

    refactoring SimpleBeforeAdvice class

--- src/main/java/ch5/SimpleBeforeAdvice.java
+++ src/main/java/ch5/SimpleBeforeAdvice.java
@@ -6,18 +6,6 @@ import org.springframework.aop.framework.ProxyFactory;
 import java.lang.reflect.Method;
 
 public class SimpleBeforeAdvice implements MethodBeforeAdvice {
-    public static void main(String[] args) {
-        MessageWriter target = new MessageWriter();
-
-        ProxyFactory pf = new ProxyFactory();
-        pf.addAdvice(new SimpleBeforeAdvice());
-        pf.setTarget(target);
-
-        MessageWriter proxy = (MessageWriter) pf.getProxy();
-        proxy.writeMessage();
-
-    }
-
     @Override
     public void before(Method method, Object[] objects, Object o) throws Throwable {
         System.out.println("Before method: " + method.getName());
