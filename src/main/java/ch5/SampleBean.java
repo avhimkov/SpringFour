735a0bccf8d585d94237cfe8ac62644d2e54bf8f
avhimkov <avhimkov@mail.ru>
2017-03-20 16:15:10 +0500

    refactoring SampleBean class

--- src/main/java/ch5/SampleBean.java
+++ src/main/java/ch5/SampleBean.java
@@ -1,10 +1,13 @@
 package ch5;
 
 public class SampleBean {
-    public void foo(int x){
-        System.out.println("Invoked foo() witch " + x);
+    public String getName(){
+        return "Chris Schaefer";
     }
-    public void bar (){
-        System.out.println("Invoked bar()");
+    public void setName (String name){
+    }
+
+    public int getAge(){
+        return 32;
     }
 }
