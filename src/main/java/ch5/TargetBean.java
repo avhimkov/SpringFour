50b3cb9fb11b3679fc7b046fd2e2783436cd51e9
avhimkov <avhimkov@mail.ru>
2017-03-21 19:36:26 +0500

    add TargetBean class

---
+++ src/main/java/ch5/TargetBean.java
@@ -0,0 +1,13 @@
+package ch5;
+
+public class TargetBean {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
