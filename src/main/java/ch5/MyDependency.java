43a634cb57d49d536116b03624de01a6ac3a9844
avhimkov <avhimkov@mail.ru>
2017-03-22 22:55:07 +0500

    add MyDependency class

---
+++ src/main/java/ch5/MyDependency.java
@@ -0,0 +1,10 @@
+package ch5;
+
+public class MyDependency {
+    public void foo(){
+        System.out.println("foo() ");
+    }
+    public void bar(){
+        System.out.println("bar() ");
+    }
+}
