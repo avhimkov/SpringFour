90c6a5d7bcd7a7e64992e2098f3ab5d060006207
avhimkov <avhimkov@mail.ru>
2017-03-24 23:33:56 +0500

    refactoring MyDependency class

--- src/main/java/ch5/MyDependency.java
+++ src/main/java/ch5/MyDependency.java
@@ -2,7 +2,7 @@ package ch5;
 
 public class MyDependency {
     public void foo(){
-        System.out.println("foo() ");
+        System.out.println("foo(int): " + intValue);
     }
     public void bar(){
         System.out.println("bar() ");
