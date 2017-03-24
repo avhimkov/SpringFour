be6a039209c1d59ccefd27d6b87e6497895a0d60
avhimkov <avhimkov@mail.ru>
2017-03-25 00:01:57 +0500

    refactoring MyAdvice class

--- src/main/java/ch5/MyAdvice.java
+++ src/main/java/ch5/MyAdvice.java
@@ -3,7 +3,11 @@ package ch5;
 import org.aspectj.lang.JoinPoint;
 
 public class MyAdvice {
-    public void simpleBeforeAdvice(JoinPoint joinPoint){
-        System.out.println("Executing" + joinPoint.getSignature().getDeclaringTypeName() + " " + joinPoint.getSignature().getName());
+    public void simpleBeforeAdvice(JoinPoint joinPoint, int intValue){
+        if (intValue != 100){
+            System.out.println("Executing" + joinPoint.getSignature().getDeclaringTypeName() + " " + joinPoint.getSignature().getName()
+            + "argument: " + intValue);
+        }
+
     }
 }
