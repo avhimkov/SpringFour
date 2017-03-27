dd29fb8fc9648ea8bf89b792c9da092603094e5f
avhimkov <avhimkov@mail.ru>
2017-03-27 22:52:41 +0500

    refactoring MyAdvice class

--- src/main/java/ch5/MyAdvice.java
+++ src/main/java/ch5/MyAdvice.java
@@ -1,6 +1,7 @@
 package ch5;
 
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
 
 public class MyAdvice {
     public void simpleBeforeAdvice(JoinPoint joinPoint, int intValue){
@@ -8,6 +9,14 @@ public class MyAdvice {
             System.out.println("Executing" + joinPoint.getSignature().getDeclaringTypeName() + " " + joinPoint.getSignature().getName()
             + "argument: " + intValue);
         }
-
+    }
+    public Object simpleAroundAdvice(ProceedingJoinPoint pjp, int intValue) throws Throwable{
+        System.out.println("Before execution: " + pjp.getSignature().getDeclaringTypeName()
+                + " " + pjp.getSignature().getName() +
+                "argument" + intValue);
+        Object retVal = pjp.proceed();
+        System.out.println("After execution: " + pjp.getSignature().getDeclaringTypeName() + " " + pjp.getSignature().getName() +
+        "argument: " + intValue);
+        return retVal;
     }
 }
