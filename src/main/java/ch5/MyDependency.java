b6d918f479f230b2eede1243c8e275842f471f43
Dmitry <avhimkov@mail.ru>
2017-03-28 18:33:18 +0500

    refactoring MyDependency class

--- src/main/java/ch5/MyDependency.java
+++ src/main/java/ch5/MyDependency.java
@@ -1,10 +1,13 @@
 package ch5;
 
+import org.springframework.stereotype.Component;
+
+@Component("myDependency")
 public class MyDependency {
     public void foo(int intValue){
         System.out.println("foo(int): " + intValue);
     }
     public void bar(){
-        System.out.println("bar() ");
+        System.out.println("bar()");
     }
 }
