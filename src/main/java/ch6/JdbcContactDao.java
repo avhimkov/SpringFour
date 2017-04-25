a44db4e1596411a447b436ed8e9038438cf46c59
Dmitry <avhimkov@mail.ru>
2017-04-25 23:14:03 +0500

    add H2 in maven and refactoring app-contex-xml

--- src/main/java/ch6/JdbcContactDao.java
+++ src/main/java/ch6/JdbcContactDao.java
@@ -1,10 +1,19 @@
 package ch6;
 
+import org.springframework.beans.factory.BeanCreationException;
+
 import javax.sql.DataSource;
 
 public class JdbcContactDao implements ContactDao {
     private DataSource dataSource;
     public void setDataSource(DataSource dataSource){
         this.dataSource = dataSource;
+    }
+
+    @Override
+    public void afterPropertiesSet() throws Exception{
+        if (dataSource==null){
+            throw new BeanCreationException("Must set dataSource on ContactDao");
+        }
     }
 }
