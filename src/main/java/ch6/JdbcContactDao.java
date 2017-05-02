031a126b333412a45d63082045698658dd598cac
Dmitry <avhimkov@mail.ru>
2017-05-02 22:32:12 +0500

    refactoring JdbcContactDao

--- src/main/java/ch6/JdbcContactDao.java
+++ src/main/java/ch6/JdbcContactDao.java
@@ -2,10 +2,14 @@ package ch6;
 
 import org.springframework.beans.factory.BeanCreationException;
 import org.springframework.beans.factory.InitializingBean;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 
 import javax.sql.DataSource;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class JdbcContactDao implements ContactDao, InitializingBean {
@@ -21,6 +25,12 @@ public class JdbcContactDao implements ContactDao, InitializingBean {
         return namedParameterJdbcTemplate.queryForObject(sql, nameParametrs, String.class);
     }
 
+    @Override
+    public List<Contact> findAll(){
+        String sql = "select id, first_name, last_name, birh_data from contact";
+        return namedParameterJdbcTemplate.query(sql, new ContactMapper());
+    }
+
     public void setDataSource(DataSource dataSource){
         this.dataSource = dataSource;
         NamedParameterJdbcTemplate namedParameterJdbcTemplate = new NamedParameterJdbcTemplate(dataSource);
@@ -34,6 +44,19 @@ public class JdbcContactDao implements ContactDao, InitializingBean {
         }
         if (namedParameterJdbcTemplate==null){
             throw new BeanCreationException("Null namedParameterJdbcTemplate on ContactDao");
+        }
+    }
+
+    public static final class ContactMapper implements RowMapper<Contact> {
+        @Override
+        public Contact mapRow(ResultSet rs, int rowNumm) throws SQLException {
+            Contact contact = new Contact();
+            contact.setId(rs.getLong("id"));
+            contact.setFirstName(rs.getString("first_name"));
+            contact.setLastName(rs.getString("last_name"));
+            contact.setBirthDate(rs.getDate("birth_name"));
+
+            return contact;
         }
     }
 }
