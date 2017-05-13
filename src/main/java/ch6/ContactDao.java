78e824e122d2506b4c902cbbfc0d07815bdaa926
Dmitry <avhimkov@mail.ru>
2017-05-13 23:03:50 +0500

    refactoring interface ContactDao

--- src/main/java/ch6/ContactDao.java
+++ src/main/java/ch6/ContactDao.java
@@ -3,7 +3,11 @@ package ch6;
 import java.util.List;
 
 public interface ContactDao {
+    List<Contact> findAll();
+    List<Contact> findByFirstName(String firstName);
     String findLastNameById(Long id);
     List<Contact> findAllWithDetail();
-
+    void insert(Contact contact);
+    void insertWithDetails(Contact contact);
+    void update(Contact contact);
 }
