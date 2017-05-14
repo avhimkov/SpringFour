289de7788106e0390e14c2b9a65f7b6a23efa8f3
Dmitry <avhimkov@mail.ru>
2017-05-14 18:26:37 +0500

    add SelectAllContacts

---
+++ src/main/java/ch6/SelectAllContacts.java
@@ -0,0 +1,25 @@
+package ch6;
+
+import org.springframework.jdbc.object.MappingSqlQuery;
+
+import javax.sql.DataSource;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class SelectAllContacts extends MappingSqlQuery<Contact> {
+    private static final String SQL_SELECT_ALL_CONTACT = "select id, first_name, last_name, birth_data from contact";
+
+    public SelectAllContacts(DataSource dataSource){
+        super(dataSource, SQL_SELECT_ALL_CONTACT);
+    }
+
+    @Override
+    protected Contact mapRow(ResultSet rs, int rowNum) throws SQLException {
+        Contact contact = new Contact();
+        contact.setId(rs.getLong("id"));
+        contact.setFirstName(rs.getString("first_name"));
+        contact.setLastName(rs.getString("last_name"));
+        contact.setBirthDate(rs.getDate("birth_name"));
+        return contact;
+    }
+}
