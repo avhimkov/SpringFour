956dea0e602615a35393ee5d95dd5a0928c43cf4
Dmitry <avhimkov@mail.ru>
2017-05-15 22:18:16 +0500

    add AnnotationJdbcDaoSample

--- src/main/java/ch6/JdbcContactDao.java
+++ src/main/java/ch6/JdbcContactDao.java
@@ -2,22 +2,10 @@ package ch6;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.springframework.beans.factory.BeanCreationException;
-import org.springframework.beans.factory.InitializingBean;
-import org.springframework.dao.DataAccessException;
-import org.springframework.jdbc.core.ResultSetExtractor;
-import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.stereotype.Repository;
-
-
 import javax.annotation.Resource;
 import javax.sql.DataSource;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 @Repository("contactDao")
 public class JdbcContactDao implements ContactDao {
@@ -49,8 +37,6 @@ public class JdbcContactDao implements ContactDao {
     public void update(Contact contact){
     }
 
-
-    //    private NamedParameterJdbcTemplate namedParameterJdbcTemplate;
     @Resource(name = "dataSource")
     public void setDataSource(DataSource dataSource) {
         this.dataSource = dataSource;
