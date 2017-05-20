b65ec397c7a4c45077fd739bf4c2d98beb07c7af
Dmitry <avhimkov@mail.ru>
2017-05-20 22:34:34 +0500

    AnnotationJdbcDaoSample refactoring

--- src/main/java/ch6/AnnotationJdbcDaoSample.java
+++ src/main/java/ch6/AnnotationJdbcDaoSample.java
@@ -1,6 +1,9 @@
 package ch6;
 
 import org.springframework.context.support.GenericXmlApplicationContext;
+
+import java.util.Date;
+import java.util.GregorianCalendar;
 import java.util.List;
 
 public class AnnotationJdbcDaoSample {
@@ -10,8 +13,14 @@ public class AnnotationJdbcDaoSample {
         ctx.refresh();
 
         ContactDao contactDao = ctx.getBean("contactDao", ContactDao.class);
-        List<Contact> contacts = contactDao.findByFirstName("Chris");
-        listContacts(contacts);
+        Contact contact = new Contact();
+        contact.getId(1l);
+        contact.getFirstName("Chris");
+        contact.getLastName("John");
+        contact.getLastName(new Date(new GregorianCalendar(1977, 10 ,1).getTime().getTime()));
+        contactDao.update(contact);
+
+        listContacts(contact.findAll());
     }
 
     private static void listContacts(List<Contact> contacts){
