b680a5de7a03c8ca7f8e031abc8725ca013e483e
Dmitry <avhimkov@mail.ru>
2017-05-22 13:12:35 +0500

    refactoring JdbcContactDao

--- src/main/java/ch6/JdbcContactDao.java
+++ src/main/java/ch6/JdbcContactDao.java
@@ -2,11 +2,17 @@ package ch6;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.springframework.dao.DataAccessException;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.ResultSetExtractor;
 import org.springframework.jdbc.support.GeneratedKeyHolder;
 import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.stereotype.Repository;
 import javax.annotation.Resource;
 import javax.sql.DataSource;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -20,6 +26,7 @@ public class JdbcContactDao implements ContactDao {
     private SelectContactByFirstName selectContactByFirstName;
     private UpdateContact updateContact;
     private InsertContact insertContact;
+    private InsertContactTelDetail insertContactTelDetail;
 
     @Override
     public List<Contact> findAll(){
@@ -40,7 +47,11 @@ public class JdbcContactDao implements ContactDao {
 
     @Override
     public List<Contact> findAllWithDetail() {
-        return null;
+        JdbcTemplate jdbcTemplate = new JdbcTemplate(getDataSource());
+        String sql = "select c.id, c.first_name, c.last_name, c.birth_date" +
+                ", t.id as contact_tel_id, t.tel_type, t.tel_number from contact c" +
+                "left join contact_tel_detail t on c.id = t.contact_id";
+        return jdbcTemplate.query(sql, new ContactTelDetailExtractor());
     }
 
     @Override
@@ -59,7 +70,30 @@ public class JdbcContactDao implements ContactDao {
 
     @Override
     public void insertWithDetail(Contact contact) {
+        insertContactTelDetail = new InsertContactTelDetail(dataSource);
+        Map<String, Object> paramMap = new HashMap<String, Object>();
+        paramMap.put("first_name", contact.getFirstName());
+        paramMap.put("last_name", contact.getLastName());
+        paramMap.put("birth_date", contact.getBirthDate());
 
+        KeyHolder keyHolder = new GeneratedKeyHolder();
+        insertContact.updateByNamedParam(paramMap, keyHolder);
+        contact.setId(keyHolder.getKey().longValue());
+        LOG.info("New contact inserted with id: " + contact.getId());
+
+        List<ContactTelDetail> contactTelDetails = contact.getContactTelDetails();
+
+        if (contactTelDetails != null){
+            for (ContactTelDetail contactTelDetail: contactTelDetails){
+                paramMap = new HashMap<String, Object>();
+                paramMap.put("contact_id", contact.getId());
+                paramMap.put("tel_type", contactTelDetail.getTelType());
+                paramMap.put("tel_number", contactTelDetail.getTelNumber());
+                insertContactTelDetail.updateByNamedParam(paramMap);
+
+            }
+        }
+        insertContactTelDetail.flush();
     }
 
     @Override
@@ -91,5 +125,37 @@ public class JdbcContactDao implements ContactDao {
 
     public DataSource getDataSource() {
         return dataSource;
+    }
+
+    private static final class ContactTelDetailExtractor implements ResultSetExtractor<List<Contact>>{
+        @Override
+        public List<Contact> extractData(ResultSet rs) throws SQLException, DataAccessException {
+            Map<Long, Contact> map = new HashMap<Long, Contact>();
+            Contact contact = null;
+            while (rs.next()){
+                Long id = rs.getLong("id");
+                contact = map.get(id);
+
+                if (contact == null){
+                    contact = new Contact();
+                    contact.setId(id);
+                    contact.setFirstName(rs.getString("first_name"));
+                    contact.setLastName(rs.getString("last_name"));
+                    contact.setBirthDate(rs.getDate("birth_date"));
+                    contact.setContactTelDetails(new ArrayList<ContactTelDetail>());
+                    map.put(id, contact);
+                }
+                Long contactTelDetailId = rs.getLong("contact_tel_id");
+                if (contactTelDetailId >0) {
+                    ContactTelDetail contactTelDetail = new ContactTelDetail();
+                    contactTelDetail.setId(contactTelDetailId);
+                    contactTelDetail.setContactId(id);
+                    contactTelDetail.setTelType(rs.getString("tel_type"));
+                    contactTelDetail.setTelNumber(rs.getString("tel_number"));
+                    contact.getContactTelDetails().add(contactTelDetail);
+                }
+            }
+            return new ArrayList<Contact>(map.values());
+        }
     }
 }
