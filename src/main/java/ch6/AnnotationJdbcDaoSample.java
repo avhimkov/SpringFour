d0a49f5573eee8fa8de50993bf42ce585b828c80
Dmitry <avhimkov@mail.ru>
2017-05-23 21:30:26 +0500

    refactoring AnnotationJdbcDaoSample

--- src/main/java/ch6/AnnotationJdbcDaoSample.java
+++ src/main/java/ch6/AnnotationJdbcDaoSample.java
@@ -2,6 +2,7 @@ package ch6;
 
 import org.springframework.context.support.GenericXmlApplicationContext;
 
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.GregorianCalendar;
 import java.util.List;
@@ -14,12 +15,22 @@ public class AnnotationJdbcDaoSample {
 
         ContactDao contactDao = ctx.getBean("contactDao", ContactDao.class);
         Contact contact = new Contact();
-        contact.setFirstName("Rod");
+        contact.setFirstName("Michael");
         contact.setLastName("Johnson");
-        contact.setLastName(new Date(new GregorianCalendar(2001, 10 ,1).getTime().getTime()));
-        contactDao.insert(contact);
+        contact.setLastName(new Date(new GregorianCalendar(1964, 10 ,1).getTime().getTime()));
+        List<ContactTelDetail> contactTelDetails = new ArrayList<ContactTelDetail>();
+        ContactTelDetail contactTelDetail = new ContactTelDetail();
+        contactTelDetail.setTelType("Home");
+        contactTelDetail.setTelNumber("11111111");
+        contactTelDetails.add(contactTelDetail);
+        contactTelDetail = new ContactTelDetail();
+        contactTelDetail.setTelType("Mobile");
+        contactTelDetail.setTelNumber("2222222");
+        contactTelDetails.add(contactTelDetail);
+        contact.setContactTelDetails(contactTelDetails);
+        contactDao.insertWithDetail(contact);
 
-        listContacts(contactDao.findAll());
+        listContacts(contactDao.findAllWithDetail());
     }
 
     private static void listContacts(List<Contact> contacts){
