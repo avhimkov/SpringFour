aa1a1218bc863a4b4f5acb8476a235b09f8a18c0
Dmitry <avhimkov@mail.ru>
2017-06-02 21:00:20 +0500

    add ContactDaoImpl class

---
+++ src/main/java/ch7/ContactDaoImpl.java
@@ -0,0 +1,26 @@
+package ch7;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.hibernate.SessionFactory;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.annotation.Resource;
+
+@Transactional
+@Repository("ContactDao")
+public class ContactDaoImpl implements ContactDao {
+    public static final Log LOG = LogFactory(ContactDaoImpl.class);
+
+    private SessionFactory sessionFactory;
+
+    public SessionFactory getSessionFactory() {
+        return sessionFactory;
+    }
+
+    @Resource(name = "sessionFactory")
+    public void setSessionFactory(SessionFactory sessionFactory) {
+        this.sessionFactory = sessionFactory;
+    }
+}
