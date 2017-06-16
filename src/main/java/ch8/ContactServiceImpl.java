e7cb786a78bb0299a32ec1b513dd56732cc1ad12
Dmitry <avhimkov@mail.ru>
2017-06-16 17:23:54 +0500

    refactoring ContactServiceImpl

--- src/main/java/ch8/ContactServiceImpl.java
+++ src/main/java/ch8/ContactServiceImpl.java
@@ -45,11 +45,18 @@ public class ContactServiceImpl implements ContactService{
 
     @Override
     public Contact save(Contact contact) {
-        return null;
+        if (contact.getId()==null){
+            log.info("Inserting new contact");
+            em.persist(contact);
+        } else {
+            em.merge(contact);
+            log.info("Updating existing contact");
+        }
+        log.info("Contact saved with id: " + contact.getId());
+        return contact;
     }
 
     @Override
     public void delete(Contact contact) {
-
     }
 }
