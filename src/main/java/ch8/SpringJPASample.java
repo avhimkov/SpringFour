97bb8c59c96458cde81446002b85053464c5b652
Dmitry <avhimkov@mail.ru>
2017-06-17 14:27:18 +0500

    refactoring SpringJPASample

--- src/main/java/ch8/SpringJPASample.java
+++ src/main/java/ch8/SpringJPASample.java
@@ -4,6 +4,7 @@ import org.springframework.context.support.GenericXmlApplicationContext;
 
 import java.util.Date;
 import java.util.List;
+import java.util.Set;
 
 public class SpringJPASample {
     public static void main(String[] args) {
@@ -12,17 +13,36 @@ public class SpringJPASample {
         ctx.refresh();
 
         ContactService contactService = ctx.getBean("jpaContactService", ContactService.class);
-        Contact contact = new Contact();
-        contact.setFirstName("Michael");
-        contact.setLastName("Jackson");
-        contact.setBirthDate(new Date());
+        Contact contact = contactService.findById(1l);
+        System.out.println("");
 
-        ContactTelDetail contactTelDetail = new ContactTelDetail("Home", "1111111111");
-        contact.addContactTelDetail(contactTelDetail);
-        contactTelDetail = new ContactTelDetail("Mobile", "2222222222");
-        contact.addContactTelDetail(contactTelDetail);
+        System.out.println("Contact with id 1:" + contact);
+
+        System.out.println("");
+
+        contact.setFirstName("Justin");
+
+        Set<ContactTelDetail> contactTels = contact.getContactTelDetails();
+        ContactTelDetail toDeleteContactTel = null;
+        for (ContactTelDetail contactTel: contactTels){
+            if (contactTel.getTelType().equals("Home")){
+                toDeleteContactTel = contactTel;
+            }
+        }
+
+        contactTels.remove(toDeleteContactTel);
         contactService.save(contact);
         listContactsWithDetail(contactService.findAllWithDetail());
+
+//        contact.setLastName("Jackson");
+//        contact.setBirthDate(new Date());
+
+//        ContactTelDetail contactTelDetail = new ContactTelDetail("Home", "1111111111");
+//        contact.addContactTelDetail(contactTelDetail);
+//        contactTelDetail = new ContactTelDetail("Mobile", "2222222222");
+//        contact.addContactTelDetail(contactTelDetail);
+//        contactService.save(contact);
+//        listContactsWithDetail(contactService.findAllWithDetail());
     }
 
     public static void listContactsWithDetail (List<Contact> contacts){
