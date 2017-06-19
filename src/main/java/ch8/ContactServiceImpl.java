3240e22165aead17dca962b34022b62976f27d61
Dmitry <avhimkov@mail.ru>
2017-06-19 15:23:52 +0500

    refactoring interface ContactService

--- src/main/java/ch8/ContactServiceImpl.java
+++ src/main/java/ch8/ContactServiceImpl.java
@@ -28,70 +28,80 @@ public class ContactServiceImpl implements ContactService {
         return null;
     }
 
-    @Transactional(readOnly = true)
     @Override
-    public List<Contact> findAllWithDetail() {
-        List<Contact> contacts = em.createNamedQuery("Contact.findAllWithDetail", Contact.class).getResultList();
-        return contacts;
-    }
-
-    @Transactional(readOnly = true)
-    @Override
-    public Contact findById(Long id) {
-        TypedQuery<Contact> query = em.createNamedQuery("Contatc.findById", Contact.class);
-        query.setParameter("id", id);
-        return query.getSingleResult();
-
+    public List<Contact> findByFirstName(String firstName) {
+        return null;
     }
 
     @Override
-    public Contact save(Contact contact) {
-        if (contact.getId() == null) {
-            log.info("Inserting new contact");
-            em.persist(contact);
-        } else {
-            em.merge(contact);
-            log.info("Updating existing contact");
-        }
-        log.info("Contact saved with id: " + contact.getId());
-        return contact;
+    public List<Contact> findByFirstNameAndLastName(String firstName, String lastnameName) {
+        return null;
     }
 
-    @Override
-    public void delete(Contact contact) {
-        Contact mergedContact = em.merge(contact);
-        em.remove(mergedContact);
+//    @Transactional(readOnly = true)
+//    @Override
+//    public List<Contact> findAllWithDetail() {
+//        List<Contact> contacts = em.createNamedQuery("Contact.findAllWithDetail", Contact.class).getResultList();
+//        return contacts;
+//    }
+//
+//    @Transactional(readOnly = true)
+//    @Override
+//    public Contact findById(Long id) {
+//        TypedQuery<Contact> query = em.createNamedQuery("Contatc.findById", Contact.class);
+//        query.setParameter("id", id);
+//        return query.getSingleResult();
+//
+//    }
 
-        log.info("Contact with id: " + contact.getId() + "deleted successfully");
-    }
+//    @Override
+//    public Contact save(Contact contact) {
+//        if (contact.getId() == null) {
+//            log.info("Inserting new contact");
+//            em.persist(contact);
+//        } else {
+//            em.merge(contact);
+//            log.info("Updating existing contact");
+//        }
+//        log.info("Contact saved with id: " + contact.getId());
+//        return contact;
+//    }
+//
+//    @Override
+//    public void delete(Contact contact) {
+//        Contact mergedContact = em.merge(contact);
+//        em.remove(mergedContact);
+//
+//        log.info("Contact with id: " + contact.getId() + "deleted successfully");
+//    }
+//
+//    @Transactional(readOnly = true)
+//    @Override
+//    public List<Contact> findAllByNativeQuery() {
+//        return em.createNamedQuery(ALL_CONTACT_NATIVE_QUERY, Contact.class).getResultList();
+//    }
 
-    @Transactional(readOnly = true)
-    @Override
-    public List<Contact> findAllByNativeQuery() {
-        return em.createNamedQuery(ALL_CONTACT_NATIVE_QUERY, Contact.class).getResultList();
-    }
-
-    @Transactional(readOnly = true)
-    @Override
-    public List<Contact> findByCriteriaQuery(String firstName, String lastName) {
-        log.info("Finding contact for firstNarne:" + firstName + " and lastName: " + lastName);
-        CriteriaBuilder cb = em.getCriteriaBuilder();
-        CriteriaQuery<Contact> criteriaQuery = cb.createQuery(Contact.class);
-        Root<Contact> contactRoot = criteriaQuery.from(Contact.class);
-        contactRoot.fetch(Contact_.contactTelDetails, JoinType.LEFT);
-        contactRoot.fetch(Contact_.hobbies, JoinType.LEFT);
-        criteriaQuery.select(contactRoot).distinct(true);
-        Predicate criteria = cb.conjunction();
-
-        if (firstName != null) {
-            Predicate p = cb.equal(contactRoot.get(Contact_.firstName), firstName);
-            criteria = cb.and(criteria, p);
-        }
-        if (lastName != null) {
-            Predicate p = cb.equal(contactRoot.get(Contact_.lastName), lastName);
-            criteria = cb.and(criteria, p);
-        }
-        criteriaQuery.where(criteria);
-        return em.createQuery(criteriaQuery).getResultList();
-    }
+//    @Transactional(readOnly = true)
+//    @Override
+//    public List<Contact> findByCriteriaQuery(String firstName, String lastName) {
+//        log.info("Finding contact for firstNarne:" + firstName + " and lastName: " + lastName);
+//        CriteriaBuilder cb = em.getCriteriaBuilder();
+//        CriteriaQuery<Contact> criteriaQuery = cb.createQuery(Contact.class);
+//        Root<Contact> contactRoot = criteriaQuery.from(Contact.class);
+//        contactRoot.fetch(Contact_.contactTelDetails, JoinType.LEFT);
+//        contactRoot.fetch(Contact_.hobbies, JoinType.LEFT);
+//        criteriaQuery.select(contactRoot).distinct(true);
+//        Predicate criteria = cb.conjunction();
+//
+//        if (firstName != null) {
+//            Predicate p = cb.equal(contactRoot.get(Contact_.firstName), firstName);
+//            criteria = cb.and(criteria, p);
+//        }
+//        if (lastName != null) {
+//            Predicate p = cb.equal(contactRoot.get(Contact_.lastName), lastName);
+//            criteria = cb.and(criteria, p);
+//        }
+//        criteriaQuery.where(criteria);
+//        return em.createQuery(criteriaQuery).getResultList();
+//    }
 }
--- src/main/java/ch8/SpringJPASample.java
+++ src/main/java/ch8/SpringJPASample.java
@@ -13,8 +13,8 @@ public class SpringJPASample {
         ctx.refresh();
 
         ContactService contactService = ctx.getBean("jpaContactService", ContactService.class);
-        List<Contact> contacts = contactService.findByCriteriaQuery("John","Smith");
-        listContactsWithDetail(contacts);
+//        List<Contact> contacts = contactService.findByCriteriaQuery("John","Smith");
+//        listContactsWithDetail(contacts);
 //        contact.setLastName("Jackson");
 //        contact.setBirthDate(new Date());
 
