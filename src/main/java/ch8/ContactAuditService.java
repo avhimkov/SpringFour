cb0c1fdd782a933bc53bf40682240e15743d0717
Dmitry <avhimkov@mail.ru>
2017-06-20 19:28:49 +0500

    add interface ContactAuditService

---
+++ src/main/java/ch8/ContactAuditService.java
@@ -0,0 +1,9 @@
+package ch8;
+
+import java.util.List;
+
+public interface ContactAuditService {
+    List<ContactAudit> findAll();
+    ContactAudit findById(Long id);
+    ContactAudit save(ContactAudit contact);
+}
