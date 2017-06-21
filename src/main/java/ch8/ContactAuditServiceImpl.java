facd1ae3c20bb5678db012692edf1efeb711a206
Dmitry <avhimkov@mail.ru>
2017-06-21 18:33:56 +0500

    add class ContactAuditServiceImpl

---
+++ src/main/java/ch8/ContactAuditServiceImpl.java
@@ -0,0 +1,30 @@
+package ch8;
+
+import com.google.common.collect.Lists;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service("contactAuditService")
+@Repository
+@Transactional
+public class ContactAuditServiceImpl implements ContactAuditService{
+    @Autowired
+    private ContactAuditRepository contactAuditRepository;
+
+    @Transactional(readOnly = true)
+    public List<ContactAudit> findAll() {
+        return Lists.newArrayList(contactAuditRepository.findAll());
+    }
+
+    public ContactAudit findById(Long id) {
+        return contactAuditRepository.findOne(id);
+    }
+
+    public ContactAudit save(ContactAudit contact) {
+        return contactAuditRepository.save(contact);
+    }
+}
