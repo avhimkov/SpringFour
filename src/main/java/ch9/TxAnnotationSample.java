1af937d2cf0ef64871efb4ff81aa7b1300d17d35
Dmitry <avhimkov@mail.ru>
2017-06-25 13:05:16 +0500

    add TxAnnotationSample ch9

---
+++ src/main/java/ch9/TxAnnotationSample.java
@@ -0,0 +1,19 @@
+package ch9;
+
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+import java.util.List;
+
+public class TxAnnotationSample {
+    public static void main(String[] args) {
+        GenericXmlApplicationContext ctx = new GenericXmlApplicationContext();
+        ctx.load("classpath:META-INF/tx-app-context-annotation.xml");
+        ctx.refresh();
+        ContactService contactService = ctx.getBean("contactService", ContactService.class);
+
+        List<Contact> contacts = contactService.findAll();
+        for (Contact contactTemp: contacts){
+            System.out.println(contactTemp);
+        }
+    }
+}
