69ffc2fbc2b9a62a8946a877e2c2a05009f885ff
Dmitry <avhimkov@mail.ru>
2017-06-27 20:43:26 +0500

    add TxDeclarativeSample

---
+++ src/main/java/ch9/TxDeclarativeSample.java
@@ -0,0 +1,25 @@
+package ch9;
+
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+import java.util.List;
+
+public class TxDeclarativeSample {
+    public static void main(String[] args) {
+        GenericXmlApplicationContext ctx = new GenericXmlApplicationContext();
+        ctx.load("classpath:META-INF/tx-declarative-app-context.xml");
+        ctx.refresh();
+        ContactService contactService = ctx.getBean("contactService", ContactService.class);
+
+        List<Contact> contacts = contactService.findAll();
+        for (Contact contactTemp: contacts){
+            System.out.println(contactTemp);
+        }
+        Contact contact = contactService.findById(1L);
+        contact.setFirstName("Peter");
+        contactService.save(contact);
+        System.out.println("Contact saved successfully");
+        System.out.println("Contact count: " + contactService.countAll());
+
+    }
+}
