29d1136ecedc2a7ba896584c092209ad687227c7
Dmitry <avhimkov@mail.ru>
2017-06-27 21:23:06 +0500

    add class TxProgrammaticSample

--- src/main/java/ch9/TxProgrammaticSample.java
+++ src/main/java/ch9/TxProgrammaticSample.java
@@ -2,25 +2,13 @@ package ch9;
 
 import org.springframework.context.support.GenericXmlApplicationContext;
 
-import java.util.List;
-
-public class TxDeclarativeSample {
+public class TxProgrammaticSample {
     public static void main(String[] args) {
         GenericXmlApplicationContext ctx = new GenericXmlApplicationContext();
         ctx.load("classpath:META-INF/tx-programmatic-app-context.xml");
         ctx.refresh();
 
         ContactService contactService = ctx.getBean("contactService", ContactService.class);
-
-        List<Contact> contacts = contactService.findAll();
-        for (Contact contactTemp: contacts){
-            System.out.println(contactTemp);
-        }
-        Contact contact = contactService.findById(1L);
-        contact.setFirstName("Peter");
-        contactService.save(contact);
-        System.out.println("Contact saved successfully");
         System.out.println("Contact count: " + contactService.countAll());
-
     }
 }
