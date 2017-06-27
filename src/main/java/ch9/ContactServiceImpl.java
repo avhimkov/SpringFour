cfdfb2a6882af848f0ea8ede22416a0538e9e0ac
Dmitry <avhimkov@mail.ru>
2017-06-27 20:38:18 +0500

    refactoring ContactServiceImpl

--- src/main/java/ch9/ContactServiceImpl.java
+++ src/main/java/ch9/ContactServiceImpl.java
@@ -1,6 +1,7 @@
 package ch9;
 
 import com.google.common.collect.Lists;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -9,18 +10,15 @@ import java.util.List;
 
 @Service("contactService")
 @Repository
-@Transactional
 public class ContactServiceImpl implements ContactService{
     private ContactRepository contactRepository;
 
     @Override
-    @Transactional(readOnly = true)
     public List<Contact> findAll() {
         return Lists.newArrayList(contactRepository.findAll());
     }
 
     @Override
-    @Transactional(readOnly = true)
     public Contact findById(Long id) {
         return contactRepository.findOne(id);
     }
@@ -31,11 +29,11 @@ public class ContactServiceImpl implements ContactService{
     }
 
     @Override
-    @Transactional(readOnly = true)
     public long countAll() {
         return contactRepository.countAllContacts();
     }
 
+    @Autowired
     public void setContactRepository(ContactRepository contactRepository) {
         this.contactRepository = contactRepository;
     }
