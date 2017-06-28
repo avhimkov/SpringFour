add16706a9231fe45f1faafd4425fa1223c13aaa
Dmitry <avhimkov@mail.ru>
2017-06-28 22:10:25 +0500

    refactoring ContactServiceImpl

--- src/main/java/ch9/ContactServiceImpl.java
+++ src/main/java/ch9/ContactServiceImpl.java
@@ -1,13 +1,8 @@
 package ch9;
 
-import com.google.common.collect.Lists;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.TransactionStatus;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.transaction.support.TransactionCallback;
-import org.springframework.transaction.support.TransactionTemplate;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
@@ -16,44 +11,41 @@ import java.util.List;
 @Service("contactService")
 @Repository
 public class ContactServiceImpl implements ContactService{
-    private ContactRepository contactRepository;
-    private TransactionTemplate transactionTemplate;
+    @PersistenceContext(unitName = "emfA")
+    private EntityManager emA;
 
-    @PersistenceContext
-    private EntityManager em;
+    @PersistenceContext(unitName = "emfB")
+    private EntityManager emB;
 
     @Override
+    @Transactional(readOnly = true)
     public List<Contact> findAll() {
-        return Lists.newArrayList(contactRepository.findAll());
+        return null;
     }
 
     @Override
+    @Transactional(readOnly = true)
     public Contact findById(Long id) {
-        return contactRepository.findOne(id);
+        return null;
     }
 
     @Override
     public Contact save(Contact contact) {
-        return contactRepository.save(contact);
+        Contact contactB = new Contact();
+        contactB.setFirstName(contact.getFirstname());
+        contactB.setLastName(contact.getLastname());
+        if (contact.getId() == null){
+            emA.persist(contact);
+            emB.persist(contactB);
+        } else {
+            emA.merge(contact);
+            emB.merge(contact);
+        }
+        return contact;
     }
 
     @Override
     public long countAll() {
-        return transactionTemplate.execute(new TransactionCallback<Long>() {
-            @Override
-            public Long doInTransaction(TransactionStatus transactionStatus) {
-                return em.createNamedQuery("Contact.countAll", Long.class).getSingleResult();
-            }
-        });
-    }
-
-    @Autowired
-    public void setContactRepository(ContactRepository contactRepository) {
-        this.contactRepository = contactRepository;
-    }
-
-    @Autowired
-    public void setTransactionTemplate(TransactionTemplate transactionTemplate) {
-        this.transactionTemplate = transactionTemplate;
+        return 0;
     }
 }
