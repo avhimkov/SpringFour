790eaacdceb7c5ce094ecd45170d4e5035542813
Dmitry <avhimkov@mail.ru>
2017-06-30 13:59:50 +0500

    add class ConvServExample

---
+++ src/main/java/ch10/ConvServExample.java
@@ -0,0 +1,13 @@
+package ch10;
+
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+public class ConvServExample {
+    public static void main(String[] args) {
+        GenericXmlApplicationContext ctx = new GenericXmlApplicationContext();
+        ctx.load("classpath:META-INF/conv-service-app-context.xml");
+        ctx.refresh();
+        Contact chris = ctx.getBean("chris", Contact.class);
+        System.out.println("Contact info: " + chris);
+    }
+}
