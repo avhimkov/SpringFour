b3c0e8d96be79b6fbbcc789bad006d29c2ed5760
Dmitry <avhimkov@mail.ru>
2017-07-01 00:39:59 +0500

    add class ContactToAnotherContactConverter and refactoring conv-service-app-context.xml

---
+++ src/main/java/ch10/ContactToAnotherContactConverter.java
@@ -0,0 +1,15 @@
+package ch10;
+
+import org.springframework.core.convert.converter.Converter;
+
+public class ContactToAnotherContactConverter implements Converter<Contact, AnotherContact> {
+    @Override
+    public AnotherContact convert(Contact contact) {
+        AnotherContact anotherContact = new AnotherContact();
+        anotherContact.setFirstName(contact.getLastName());
+        anotherContact.setLastName(contact.getFirstName());
+        anotherContact.setBirthDate(contact.getBirthDate());
+        anotherContact.setPersonalSite(contact.getPersonalSite());
+        return anotherContact;
+    }
+}
