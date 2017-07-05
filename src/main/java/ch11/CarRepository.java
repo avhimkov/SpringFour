5cfc189ccd7d5ba99dcfe61336a98cc8f92dd45d
Dmitry <avhimkov@mail.ru>
2017-07-05 20:18:36 +0500

    add interface CarRepository

---
+++ src/main/java/ch11/CarRepository.java
@@ -0,0 +1,6 @@
+package ch11;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface CarRepository extends CrudRepository<Car, Long> {
+}
--- src/main/resorces/META-INF/sql/test-data.sql
+++ src/main/resorces/META-INF/sql/test-data.sql
@@ -1,6 +1,7 @@
+USE TEST;
+SET
+master_user_profile.fellow = 'y';
 insert into car (license_plate, manufacturer, manufacture_date)
-values ('LICENSE-1001', 'Ford', '1980-07-30');
-insert into car (license_plate, manufacturer, manufacture_date)
-values ('LICENSE-1002', 'Toyota', '1992-12-30');
-insert into car (license_plate, manufacturer, manufacture_date)
-values ('LICENSE-1003', 'ВМW', '2003-1-6');
+VALUES ('LICENSE-1001', 'Ford', '1980-07-30'),
+       ('LICENSE-1002', 'Toyota', '1992-12-30'),
+       ('LICENSE-1003', 'ВМW', '2003-1-6');
