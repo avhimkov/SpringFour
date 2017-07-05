5413a5c411e70fabb1c35d36b114faa5f356f4f5
Dmitry <avhimkov@mail.ru>
2017-07-05 19:51:49 +0500

    refactoring schema.sql test-data.sql

--- src/main/resorces/META-INF/sql/test-data.sql
+++ src/main/resorces/META-INF/sql/test-data.sql
@@ -1,23 +1,6 @@
-DROP TABLE IF EXISTS CONTACT;
-CREATE TABLE CONTACT (
-  ID         INT         NOT NULL AUTO_INCREMENT
-  ,
-  FIRST_NAМE VARCHAR(60) NOT NULL
-  ,
-  LAST_NAМE  VARCHAR(40) NOT NULL
-  ,
-  BIRTH_DATE DATE
-  ,
-  VERSION    INT         NOT NULL DEFAULT 0
-  ,
-  UNIQUE UQ_CONTACT_1 (FIRST_NAМE, LAST_NAМE)
-  ,
-  PRIMARY KEY (ID)
-);
-
-insert into contact (first_name, last_name, birth_date)
-values ('Chris', 'Schaefer', '1981-05-03');
-insert into contact (first_name, last_name, birth_date)
-values ('Scott', 'Tiger', '1990-11-02');
-insert into contact (first_name, last_name, birth_date)
-values ('John', 'Smith', '1964-02-28');
+insert into car (license_plate, manufacturer, manufacture_date)
+values ('LICENSE-1001', 'Ford', '1980-07-30');
+insert into car (license_plate, manufacturer, manufacture_date)
+values ('LICENSE-1002', 'Toyota', '1992-12-30');
+insert into car (license_plate, manufacturer, manufacture_date)
+values ('LICENSE-1003', 'ВМW', '2003-1-6');
