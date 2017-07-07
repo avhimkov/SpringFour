0b3dbe9751a89cac613c6ecbfcae657a8d54637e
Dmitry <avhimkov@mail.ru>
2017-07-07 21:01:49 +0500

    add class AsyncServiceImpl

---
+++ src/main/java/ch11/AsyncServiceImpl.java
@@ -0,0 +1,37 @@
+package ch11;
+
+import org.slf4j.LoggerFactory;
+import org.slf4j.Logger;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.scheduling.annotation.AsyncResult;
+
+import java.util.concurrent.Future;
+
+public class AsyncServiceImpl implements AsyncService {
+    final Logger logger = LoggerFactory.getLogger(AsyncServiceImpl.class);
+
+    @Async
+    @Override
+    public void asyncTask() {
+        logger.info("Start execution of async. task");
+        try {
+            Thread.sleep(10000);
+        } catch (Exception ex){
+            ex.printStackTrace();
+        }
+        logger.info("Complete execution of async. task");
+    }
+
+    @Async
+    @Override
+    public Future<String> asyncWithReturn(String name) {
+        logger.info("Start execution of async. task with return");
+        try {
+            Thread.sleep(5000);
+        } catch (Exception ex){
+            ex.printStackTrace();
+        }
+        logger.info("Complete execution of async. task with return");
+        return new AsyncResult<String>("Hello: " + name);
+    }
+}
