bde8ffbc35a49df6390ca1fd1178f8bd768bf883
Dmitry <avhimkov@mail.ru>
2017-07-07 20:46:53 +0500

    refactoring CarServiceImpl

--- src/main/java/ch11/CarServiceImpl.java
+++ src/main/java/ch11/CarServiceImpl.java
@@ -6,6 +6,7 @@ import org.joda.time.Years;
 import org.slf4j.LoggerFactory;
 import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Repository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -32,7 +33,7 @@ public class CarServiceImpl implements CarService {
         return carRepository.save(car);
     }
 
-    @Override
+    @Scheduled(fixedDelay=10000)
     public void updateCarAgeJob() {
         List<Car> cars = findAll();
 
