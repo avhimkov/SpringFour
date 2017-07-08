0bc9e7be2c2fed006644340e24a4707b6b7a3b0a
Dmitry <avhimkov@mail.ru>
2017-07-09 01:49:20 +0500

    add ContactService interface

---
+++ src/main/java/ch12/ContactService.java
@@ -0,0 +1,11 @@
+package ch12;
+
+import java.util.List;
+
+public interface ContactService {
+    List<Contact> findAll();
+    List<Contact> findByFirstName(String firstName);
+    Contact findById(Long id);
+    Contact save(Contact contact);
+    void delete(Contact contact);
+}
