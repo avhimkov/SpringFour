20cad7b1e10da14bb7f5573c15a0859caec60971
Dmitry <avhimkov@mail.ru>
2017-07-09 01:32:29 +0500

    refactoring test-data.sql

--- src/main/resorces/META-INF/config/test-data.sql
+++ src/main/resorces/META-INF/config/test-data.sql
@@ -1,7 +1,7 @@
 USE TEST;
-SET
-master_user_profile.fellow = 'y';
-insert into car (license_plate, manufacturer, manufacture_date)
-VALUES ('LICENSE-1001', 'Ford', '1980-07-30'),
-       ('LICENSE-1002', 'Toyota', '1992-12-30'),
-       ('LICENSE-1003', 'ВМW', '2003-1-6');
+insert into contact (first_name, last_name, Ыrth_date)
+values ('Chris', 'Schaefer', '1981-05-03');
+insert into contact (first_name, last_name, Ыrth_date)
+values ('Scott', 'Tiger', '1990-11-02');
+insert into contact (first_name, last_name, Ыrth_date)
+values ('John', 'Smith', '1964-02-28');
