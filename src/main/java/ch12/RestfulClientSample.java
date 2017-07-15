0e75f84bf44693ade5b36b7f13692cb80f06072d
Dmitry <avhimkov@mail.ru>
2017-07-15 22:43:19 +0500

    refactoring RestfulClientSample two

--- src/main/java/ch12/RestfulClientSample.java
+++ src/main/java/ch12/RestfulClientSample.java
@@ -1,5 +1,6 @@
 package ch12;
 
+import org.joda.time.DateTime;
 import org.springframework.context.support.GenericXmlApplicationContext;
 import org.springframework.web.client.RestTemplate;
 
@@ -29,6 +30,27 @@ public class RestfulClientSample {
         contact = restTemplate.getForObject(URL_GET_CONTACT_BY_ID, Contact.class, 1);
         System.out.println(contact);
         System.out.println("");
+
+        contact = restTemplate.getForObject(URL_UPDATE_CONTACT, Contact.class, 1);
+        contact.setFirstName("John Doe");
+        System.out.println("Testing update contact bу id :");
+        restTemplate.put(URL_UPDATE_CONTACT, contact, 1);
+        System.out.println ( "Contact update successfully: " + contact);
+        System.out.println("");
+
+        restTemplate.delete(URL_DELETE_CONTACT, 1);
+        System.out.println ( "Testing delete contact bу id : ") ;
+        contacts = restTemplate.getForObject(URL_GET_ALL_CONTACTS, Contacts.class);
+        listContacts(contacts);
+
+        System.out.println("Testing create contact :");
+        Contact contactNew = new Contact();
+        contactNew.setFirstName("James");
+        contactNew.setLastName("Gosling");
+        contactNew.setBirthDate(new DateTime());
+        contactNew =
+                restTemplate.postForObject(URL_CREATE_CONTACT, contactNew, Contact.class);
+        System. out. println ( "Contact created success fully: " + contactNew) ;
     }
 
     private static void listContacts (Contacts contacts){
