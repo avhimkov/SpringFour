1afbd1509a6d925ab2daa8db3feeeb131b91bd6b
Dmitry <avhimkov@mail.ru>
2017-07-16 19:19:45 +0500

    add WeatherServiceImpl class

---
+++ src/main/java/ch12/WeatherServiceImpl.java
@@ -0,0 +1,13 @@
+package ch12;
+
+public class WeatherServiceImpl implements WeatherService {
+    @Override
+    public String getForecast(String stateCode) {
+        if ("FL".equals(stateCode)){
+            return "Hot";
+        } else if ("MA".equals(stateCode)){
+            return "Cold";
+        }
+        return "Not available at this time";
+    }
+}
