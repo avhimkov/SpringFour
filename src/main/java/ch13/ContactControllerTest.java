dbf7463a5f9ee4f7b2c31af62685f69edcfc40b9
Dmitry <avhimkov@mail.ru>
2017-07-19 00:44:20 +0500

    refactoring ContactControllerTest

--- src/main/java/ch13/ContactControllerTest.java
+++ src/main/java/ch13/ContactControllerTest.java
@@ -3,6 +3,8 @@ package ch13;
 import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
+import org.mockito.stubbing.Answer;
+import org.mockito.invocation.InvocationOnMock;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -20,7 +22,7 @@ import org.springframework.ui.ExtendedModelMap;
 public class ContactControllerTest {
     private  final List<Contact> contacts = new ArrayList<Contact>();
 
-    @Bef
+    @Before
     public void initContacts(){
         Contact contact = new Contact();
         contact.setId(1L);
@@ -40,4 +42,31 @@ public class ContactControllerTest {
         Contacts modelContacts =(Contacts) uiModel.get("contacts");
         assertEquals(1, modelContacts.getContacts().size);
     }
+
+    @Test
+    public void testCreate(){
+        final Contact newContact = new Contact();
+        newContact.setId(999L);
+        newContact.setFirstName("Rod");
+        newContact.setLastName("Johnson");
+        ContactService contactService = mock(ContactService.class);
+        when(contactService.save(newContact)).
+                thenAnswer(new Answer<Contact>() {
+                    public Contact answer(InvocationOnMock invocation)
+                            throws Throwable {
+                        contacts.add(newContact);
+                        return newContact;
+                    }
+                }) ;
+
+        ContactController contactController = new ContactController();
+        ReflectionTestUtils.setField(contactController, "contactService",
+                contactService);
+        Contact contact = contactController.create(newContact);
+        assertEquals(Long.valueOf(999L), contact.getId());
+        assertEquals("Rod", contact.getFirstName());
+        assertEquals("Johnson", contact.getLastName());
+        assertEquals(2, contacts.size());
+    }
+
 }
