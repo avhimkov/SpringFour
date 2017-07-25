d6cbc09d6f4e04cae4e95c1be7bb02fade0139cc
Dmitry <avhimkov@mail.ru>
2017-07-25 21:05:57 +0500

    add JavaScriptTest class

---
+++ src/main/java/сh14/JavaScriptTest.java
@@ -0,0 +1,18 @@
+package сh14;
+
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineManager;
+import javax.script.ScriptException;
+
+public class JavaScriptTest {
+    public static void main(String[] args) {
+        ScriptEngineManager mgr = new ScriptEngineManager();
+        ScriptEngine jsEngine = mgr.getEngineByName("JavaScript");
+
+        try {
+            jsEngine.eval("print('Hello JavaScript in Java')");
+        } catch (ScriptException ex){
+            ex.printStackTrace();
+        }
+    }
+}
