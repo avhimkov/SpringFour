617b42ac645f39eed0f11f33ac15beb6b3f8c620
Dmitry <avhimkov@mail.ru>
2017-08-03 17:29:22 +0500

    refactoring ContactController

--- src/main/java/ch16/ContactController.java
+++ src/main/java/ch16/ContactController.java
@@ -4,6 +4,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.ui.Model;
@@ -23,6 +24,14 @@ public class ContactController {
         logger.info("No of contacts: " + contacts.size());
         return "contacts/list";
     }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    public String show(@PathVariable("id") Long id, Model uiModel){
+        Contact contact = contactService.findById(id);
+        uiModel.addAttribute("contact", contact);
+        return "contacts/show";
+    }
+
 
     @Autowired
     public void setContactService(ContactService contactService){
