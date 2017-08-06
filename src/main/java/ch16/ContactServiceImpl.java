f8757353d972bf91290f98017f9b750ca92ebc26
Dmitry <avhimkov@mail.ru>
2017-08-06 13:33:17 +0500

    refactoring ContactServiceImpl class

--- src/main/java/ch16/ContactServiceImpl.java
+++ src/main/java/ch16/ContactServiceImpl.java
@@ -2,6 +2,8 @@ package ch16;
 
 import com.google.common.collect.Lists;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Repository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -35,5 +37,10 @@ public class ContactServiceImpl implements ContactService {
     @Autowired
     public void setContactRepository(ContactRepository contactRepository){
         this.contactRepository = contactRepository;
+    }
+
+    @Override
+    public Page<Contact> findAllByPage(Pageable pageable) {
+        return contactRepository.findAll(pageable);
     }
 }
