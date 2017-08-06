d03ce49eaa45ce40669a4c9bd2c1e8153fcb0277
Dmitry <avhimkov@mail.ru>
2017-08-06 13:31:50 +0500

    refactoring interface ContactService

--- src/main/java/ch16/ContactService.java
+++ src/main/java/ch16/ContactService.java
@@ -1,9 +1,12 @@
 package ch16;
 
 import java.util.List;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 public interface ContactService {
     List<Contact> findAll();
     Contact findById(Long id);
     Contact save(Contact contact);
+    Page<Contact> findAllByPage(Pageable pageable);
 }
