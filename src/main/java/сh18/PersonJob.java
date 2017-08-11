c01e110186da047aa23209e4906265b6bb851108
Dmitry <avhimkov@mail.ru>
2017-08-11 12:51:51 +0500

    add PersonJob class

---
+++ src/main/java/сh18/PersonJob.java
@@ -0,0 +1,22 @@
+package сh18;
+
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.JobParametersBuilder;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import java.util.Date;
+
+public class PersonJob {
+    public static void main(String[] args) throws Exception {
+        ApplicationContext applicationContext = new ClassPathXmlApplicationContext(
+                "META-INF/jobs/person/personJob.mxl"
+        );
+        Job job = applicationContext.getBean(Job.class);
+        JobLauncher jobLauncher = applicationContext.getBean(JobLauncher.class);
+        JobParameters jobParameters = new JobParametersBuilder().addDate("date", new Date()).toJobParameters();
+        jobLauncher.run(job, jobParameters);
+    }
+}
